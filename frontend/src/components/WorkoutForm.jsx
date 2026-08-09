6da3387096aa35a30d105161f491c9846e93221f
jatinkm007 <jatinkumishra693@gmail.com>
2026-08-10 01:18:33 +0530

    Import cors in server.js

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -18,7 +18,7 @@ const WorkoutForm = () => {
         
         const workout = {title, load, reps}
 
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/workouts`, {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/workouts/`, {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
