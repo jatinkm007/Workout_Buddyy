9afc716590110a0ee71ba502abda8b9417e81f58
jatinkm007 <jatinkumishra693@gmail.com>
2026-08-21 01:15:46 +0530

    Final Set-Up

--- frontend/src/hooks/UseLogin.jsx
+++ frontend/src/hooks/UseLogin.jsx
@@ -11,7 +11,7 @@ export const useLogin = () =>{
         setIsLoading(true)
         setError(null)
 
-        const response =  await fetch('api/user/login',{
+        const response =  await fetch(`${import.meta.env.VITE_API_URL}/api/user/login`,{
             method: 'POST',
             headers: {'Content-type' : 'application/json'},
             body: JSON.stringify({email,password})
--- frontend/src/hooks/UserSignUp.jsx
+++ frontend/src/hooks/UserSignUp.jsx
@@ -11,7 +11,7 @@ export const userSignup = () =>{
         setIsLoading(true)
         setError(null)
 
-        const response =  await fetch('api/user/signup',{
+        const response =  await fetch(`${import.meta.env.VITE_API_URL}/api/user/signup`,{
             method: 'POST',
             headers: {'Content-type' : 'application/json'},
             body: JSON.stringify({username, email, password})
